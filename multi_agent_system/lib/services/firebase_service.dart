f62a3b1928162feda41618473e34e9373e4e5a9f
ahmadexe <muahmad710@gmail.com>
2025-07-14 05:02:56 +0500

    feat: config google sign in

--- multi_agent_system/lib/services/firebase_service.dart
+++ multi_agent_system/lib/services/firebase_service.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseService {
   static final _auth = FirebaseAuth.instance;
@@ -15,5 +16,43 @@ class FirebaseService {
         'Error initializing Firebase (Failed while trying to authenticate dummy uesr): $e',
       );
     }
+  }
+
+  static Future<UserCredential> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+        'https://www.googleapis.com/auth/gmail.readonly',
+        'https://www.googleapis.com/auth/calendar.events',
+        'https://www.googleapis.com/auth/gmail.send',
+      ],
+    );
+
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser == null) {
+      throw Exception('Sign-in aborted by user');
+    }
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null || idToken == null) {
+      throw Exception('Missing Google Auth Token');
+    }
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: accessToken,
+      idToken: idToken,
+    );
+
+    final userCredential = await FirebaseAuth.instance.signInWithCredential(
+      credential,
+    );
+
+    debugPrint('🟢 Signed in as: ${userCredential.user?.email}');
+    return userCredential;
   }
 }
